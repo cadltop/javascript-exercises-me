e093b965c4c9aca9dc8a4f8b30d67e49c669b287
cadltop <158861368+cadltop@users.noreply.github.com>
2024-03-06 13:53:31 -0500

    Pass the first test.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(items, value) {
+    if (items.includes(value)) {
+        function getValue(item) {
+            return item !== value;
+        } 
+        const newArray = items.filter(getValue);
+        return newArray;
+    }
 };
 
 // Do not edit below this line
