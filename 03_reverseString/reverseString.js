857f9a48e9a7675ff9a36f228aaedd44413daafc
cadltop <158861368+cadltop@users.noreply.github.com>
2024-03-05 21:39:53 -0500

    Pass the first test.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const letters = word.split('');
+    let string = "";
+    for (let i = (letters.length - 1); i >= 0; i--) {
+        string += letters[i];
+    }
+    return string;
 };
 
 // Do not edit below this line
