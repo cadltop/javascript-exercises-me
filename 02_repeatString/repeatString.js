9c2cdc869f3d5ade4b44f3b644620f6a651ad6c6
cadltop <158861368+cadltop@users.noreply.github.com>
2024-03-05 21:13:42 -0500

    Pass the fourth test.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,11 +2,16 @@
 let string, num;
 
 const repeatString = function(string, num) {
-    let string2 = string;
-    for (i = 1; i < num; i++) {
-        string2 += string;
+    if (num > 0) {
+        let string2 = string;
+        for (i = 1; i < num; i++) {
+            string2 += string;
+        }
+        return string2;
+    } else {
+        let string2 = "";
+        return string2;
     }
-    return string2;
 };
 
 // Do not edit below this line
