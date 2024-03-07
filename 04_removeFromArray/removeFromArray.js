7da9870fbaa4b47f7f823156b4a20d6406a93445
cadltop <158861368+cadltop@users.noreply.github.com>
2024-03-06 19:05:53 -0500

    Pass the fifth test.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,20 @@
 const removeFromArray = function(items, ...values) {
+    /*
     if (items.includes(...values)) {
         const newArray = items.filter(getValue => !values.includes(getValue));
         return newArray; 
     } else {
         return items;
     }
+    */
+
+    const newArr = [];
+    for (const value of values) {
+        if (items.includes(value)){
+            newArr.push(value);
+        }
+    }
+    return items.filter(getValue => !newArr.includes(getValue));
 };
 
 // Do not edit below this line
