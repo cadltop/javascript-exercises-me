1c7e67f572b4b1e23f22ba2008a629825b1f260c
cadltop <158861368+cadltop@users.noreply.github.com>
2024-03-06 21:38:10 -0500

    Delete comments.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,5 @@
 const sumAll = function(num1, num2) {
-    /*let sum=0;
-    for (num1 ; num1<=num2 ; num1++){
-        sum += num1; 
-    }
-    return sum;*/
+    
     let sum=0;
     if (num1 < 0 || num2 < 0){
         return "ERROR";
