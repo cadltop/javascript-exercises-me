625316d7d78d7a13d35b3c54ad5ac327b6eae2cf
cadltop <158861368+cadltop@users.noreply.github.com>
2024-03-06 23:11:49 -0500

    Pass Second tests on the two  functions.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const convertToCelsius = function(degrees) {
   let f = (degrees - 32) * (5 / 9);
-  return f;
+  return parseFloat(f.toFixed(1));
 };
 
 const convertToFahrenheit = function(degrees) {
   let c = (degrees * (9 / 5)) + 32;
-  return c;
+  return parseFloat(c.toFixed(1));
 };
 
 // Do not edit below this line
