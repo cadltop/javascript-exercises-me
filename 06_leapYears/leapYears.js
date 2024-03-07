be73a41f54797243d5d6a9fb04617b590ee2ebd8
cadltop <158861368+cadltop@users.noreply.github.com>
2024-03-06 21:55:43 -0500

    Pass first test.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let leap = (year % 4 === 0 || year % 400 === 0 && year % 100 !== 0)
+    ? true
+    : false;
+    return leap;
 };
 
 // Do not edit below this line
