df5e4686399b2dcb73e2aecf16ebbfa3284f4673
cadltop <158861368+cadltop@users.noreply.github.com>
2024-03-06 21:32:57 -0500

    Pass the fourth test.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -5,16 +5,20 @@ const sumAll = function(num1, num2) {
     }
     return sum;*/
     let sum=0;
+    if (num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
     if (num1 < num2){
         for (num1 ; num1<=num2 ; num1++){
             sum += num1; 
         }
+        return sum;
     } else if (num1 > num2){
         for (num2 ; num2<=num1 ; num2++){
             sum += num2; 
         }
+        return sum;
     }
-    return sum;
     
 };
 
