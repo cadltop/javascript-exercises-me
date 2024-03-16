a672c79bb0c8d45c1a609d3d3fff84b46ae372ee
cadltop <158861368+cadltop@users.noreply.github.com>
2024-03-16 15:10:40 -0500

    Pass test #2.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,7 +1,7 @@
 const fibonacci = function(member) {
     let seq = [1];
 
-    for (let i = 1; i <=8; i++) {
+    for (let i = 1; i <=26; i++) {
         if (seq[i-2] === undefined){
             seq[i] = seq[i-1] + 0;
         } else {
