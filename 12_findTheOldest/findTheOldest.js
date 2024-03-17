5a233474c75f4dfb094c380301f2c65041a18841
cadltop <158861368+cadltop@users.noreply.github.com>
2024-03-16 22:04:08 -0500

    Pass test #1.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let age = [];
 
+    for (obj in people) {
+        age[obj] = people[obj].yearOfDeath - people[obj].yearOfBirth;
+    }
+
+    let oldest =  Math.max.apply(null, age);
+
+    for (obj in people) {
+        if ((people[obj].yearOfDeath - people[obj].yearOfBirth) === oldest) {
+            return people[obj];
+        }
+    }
 };
 
 // Do not edit below this line
